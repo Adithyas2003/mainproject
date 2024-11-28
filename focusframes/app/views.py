ee42c6fea724f2e83dd164ce393e56a6f8bbad43
Adithyas2003 <adathyas49@gmail.com>
2024-11-28 17:12:46 +0530

    main updated

--- focusframes/app/views.py
+++ focusframes/app/views.py
@@ -1,25 +1,22 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login
+from django.contrib import messages
+from .models import *
+
 
 # Create your views here.
 def e_shop_login(req):
-    if 'shop' in req.session:
-        return redirect(shop_home)
-    if 'user' in req.session:
-        return redirect(user_home)
+    
     if req.method=='POST':
         uname=req.POST['uname']
         password=req.POST['password']
         data=authenticate(username=uname,password=password)
         if data:
             login(req,data)
-            if data.is_superuser:
-                req.session['shop']=uname
-                return redirect(shop_home)
-            else:
-                req.session['user']=uname
-                return redirect(user_home)
+           
         else:
             messages.warning(req, "invalid password")
             return redirect(e_shop_login)
     else:
         return render(req,'login.html')
+	
