e902c8ae11b2b9917e8241e9b0066dd74d264f6a
Adithyas2003 <adathyas49@gmail.com>
2024-11-29 17:49:33 +0530

    mainproject added

--- focusframes/app/urls.py
+++ focusframes/app/urls.py
@@ -3,4 +3,10 @@ from .import views
 
 urlpatterns = [
     path('',views.e_shop_login),
+    path('shop_home',views.shop_home),
+    path('logout',views.e_shop_logout),
+
+#######################user##################
+    path('register/',views.Register),
+    path('user_home',views.user_home),
 ]
--- focusframes/app/views.py
+++ focusframes/app/views.py
@@ -1,22 +1,70 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth import authenticate,login
+from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from .models import *
+import os
+from django.contrib.auth.models import User
 
 
 # Create your views here.
+
 def e_shop_login(req):
-    
+    if 'shop' in req.session:
+        return redirect(shop_home)
+    if 'user' in req.session:
+        return redirect(user_home)
     if req.method=='POST':
         uname=req.POST['uname']
         password=req.POST['password']
         data=authenticate(username=uname,password=password)
         if data:
             login(req,data)
-           
+            if data.is_superuser:
+                req.session['shop']=uname
+                return redirect(shop_home)
+            else:
+                req.session['user']=uname
+                return redirect(user_home)
         else:
             messages.warning(req, "invalid password")
             return redirect(e_shop_login)
     else:
         return render(req,'login.html')
 	
+
+def shop_home(req):
+    data=product.objects.all()
+    if 'shop' in req.session:
+        return render(req,'shop/home.html',{'products':data})
+    else:
+        return redirect(e_shop_login)
+
+	
+def e_shop_logout(req):
+    logout(req)
+    req.session.flush()
+    return redirect(e_shop_login)
+def Register(req):
+    if req.method=='POST':
+        username=req.POST['uname']
+        email=req.POST['email']
+        password=req.POST['pswd']
+        
+        try:
+            data=User.objects.create_user(first_name=username,email=email,username=email,password=password)
+            data.save()
+        except:
+            messages.warning(req,"username already exist")
+            return redirect(Register)
+        return redirect(e_shop_login)
+    else:
+        return render(req,'user/Register.html')
+
+
+def user_home(req):
+    if 'user' in req.session:
+        data=product.objects.all()
+        return render(req,'user/home.html',{'products':data})
+    else:
+        return redirect(e_shop_login)
+    
